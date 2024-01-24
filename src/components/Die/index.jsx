9777d17cb73178b1f8eaa6439499cc03c2349d27
AngerOverApathy <violasian@gmail.com>
2024-01-23 20:59:49 -0800

    more style added

--- src/components/Die/index.jsx
+++ src/components/Die/index.jsx
@@ -5,7 +5,7 @@ export default function Die(props) {
     return (
         <>
             <div className="die">
-                <h2>{props.value}</h2>
+                <h2 className='die-num'>{props.value}</h2>
             </div>
         </>
     )
