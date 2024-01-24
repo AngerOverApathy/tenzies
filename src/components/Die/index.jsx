15396a1b4c36640d5beda60d899c00dce44bba9c
AngerOverApathy <violasian@gmail.com>
2024-01-23 21:09:19 -0800

    more style added

--- src/components/Die/index.jsx
+++ src/components/Die/index.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import './style.css';
 
 export default function Die(props) {
+
+    function allNewDice() {
+        let newArray = [];
+        for (let i = 0; i < 10; i++) {
+            let digit = Math.floor(Math.random() * 6) + 1;
+            newArray.push(digit);
+        }
+        return newArray;
+    }
+
     return (
         <>
             <div className="die">
