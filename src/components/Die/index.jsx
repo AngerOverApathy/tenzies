f1616e5e08fd30fbc7d9ac1275146baea06a8249
AngerOverApathy <violasian@gmail.com>
2024-04-14 20:10:01 -0700

    conditional styling created

--- src/components/Die/index.jsx
+++ src/components/Die/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './style.css';
 
 export default function Die(props) {
-    // This keeps "die" as the base class, and adds "heldDie" only if isHeld is true
     const dieClass = `die ${props.isHeld ? 'heldDie' : ''}`;
 
     return (
